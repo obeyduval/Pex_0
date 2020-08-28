5ee38246b5aa26a6bf13913d5bfbf5e701e76403
obeyduval <C22Timothy.Jackson@usafa.edu>
2020-08-28 10:23:27 -0600

    create random number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
@@ -10,13 +11,15 @@ public class Main {
 
     public static void playgame(){
         String username = getusername();
+        int magicnum = randomizemagicnumber();
+        int guesses = guess(username, magicnum);
     }
 
     public static String getusername(){
 
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Welcome to the High-Low Game!\nThe goal of this game is to guess the secret number (1-100)\nEnter your username:");
+        System.out.print("Welcome to the High-Low Game!\nThe goal of this game is to guess the secret number (1-100)\nEnter your username: ");
         String playername = in.nextLine();
 
         if(playername.isEmpty()){
@@ -26,5 +29,21 @@ public class Main {
         }
 
         return playername;
+    }
+
+    public static int guess(String username, int magicnum){
+        int guesses = 0;
+
+        System.out.println("DEBUGing magic num = " +magicnum);
+
+        return guesses;
+    }
+
+    public static int randomizemagicnumber(){
+        Random number = new Random();
+
+        int magicnum = number.nextInt(100);
+
+        return magicnum;
     }
 }
