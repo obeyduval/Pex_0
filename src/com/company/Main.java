e04279a65747c8ffb55b88195efa869e39c672f6
obeyduval <C22Timothy.Jackson@usafa.edu>
2020-08-28 10:00:23 -0600

    last test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hey");
-	System.out.println("test");
-	System.out.println("Last test");
+	playgame();
+    }
+
+    public static void playgame(){
+        String username = getusername();
+    }
+
+    public static String getusername(){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Welcome to the High-Low Game!\nThe goal of this game is to guess the secret number (1-100)\nEnter your username:");
+        String playername = in.nextLine();
+
+        System.out.println("Welcome, " + playername + "!");
+
+
+
+        return playername;
     }
 }
